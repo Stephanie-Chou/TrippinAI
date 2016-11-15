21aa82895c248aaea4465c1c42632320092ff0ee
Stephanie Chou <stephanie.chou@appdynamics.com>
2016-11-15 14:47:22 -0800

    react donation project

---
+++ src/client/app/index.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+// run  ./node_modules/.bin/webpack -d
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+        this.state.annualIncome = {value: 10000};
+        this.state.percentage = {value: 1.0};
+
+        this.handleIncomeChange = this.handleIncomeChange.bind(this);
+        this.handlePercentageChange = this.handlePercentageChange.bind(this);
+
+        this.calculateAnnualDonation = this.calculateAnnualDonation.bind(this);
+        this.calculateMonthlyDonation = this.calculateMonthlyDonation.bind(this);
+
+    }
+
+    handleIncomeChange(event) {
+        this.setState({annualIncome: {value: event.target.value}});
+    }
+
+    handlePercentageChange(event) {
+        this.setState({percentage: {value: event.target.value}});
+    }
+
+    calculateAnnualDonation() {
+        return (this.state.annualIncome.value * this.state.percentage.value / 100).toFixed(2);
+    }
+
+    calculateMonthlyDonation() {
+        return (this.calculateAnnualDonation()/12).toFixed(2);
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <p> I earn <span className="number">$<NumberInput value={this.state.annualIncome.value} onChange={this.handleIncomeChange}/> </span> annually </p>
+                    <p> I can donate <span className="number"><NumberInput value={this.state.percentage.value} onChange={this.handlePercentageChange}/>% </span>annually </p>
+                </form>
+
+                <Result annualDonation={this.calculateAnnualDonation()}
+                        monthlyDonation={this.calculateMonthlyDonation()}/>
+            </div>
+
+        );
+    }
+}
+
+class Result extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="results">
+                <p>I will donate <span className="number">${this.props.annualDonation} </span> per year</p>
+                <p>That's just <span className="number">${this.props.monthlyDonation} </span> every month</p>
+
+                <button> I Donated </button>
+            </div>
+        )
+    }
+}
+
+
+class NumberInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value
+        };
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange(e) {
+
+        if (this.isValidInput(e)) {
+            this.setState({value: e.target.value});
+            this.props.onChange(e);
+        }
+
+    }
+
+    isValidInput(e) {
+        // is it a number? ignore commas because we'll fix that in the render
+        var regex = new RegExp('^\\d*.?\\d*$');
+        return e.target.value.replace(/,/g, '').match(regex);
+    }
+
+    renderValue() {
+        // put in commas if left of decimal is greater than 4 digits
+        var regex = new RegExp('^\\d*');
+        var integers = this.state.value.toString().replace(/,/g, '').match(regex)[0];
+
+        console.log(integers)
+
+        if (integers.length > 4) {
+            return integers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        } else {
+            return this.state.value;
+        }
+
+    }
+
+    render() {
+        return (
+            <input className="number" type="text" value={this.renderValue()} placeholder={this.state.value} onChange={this.handleChange}/>
+        )
+    }
+}
+
+
+render(<Form/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+    entry: './src/client/app/index.jsx',
+    output: {
+        filename: './src/client/public/bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
