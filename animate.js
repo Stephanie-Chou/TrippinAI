9207b0dfe32fc27ca20aaf52afff77bf3f358700
Stephanie-Chou <stephanie.chou@outlook.com>
2017-02-14 17:03:52 -0500

    update some resume styles for mobile

--- animate.js
+++ animate.js
@@ -71,9 +71,7 @@
     var html = document.documentElement;
     return (
       rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || html.clientHeight) &&
-      rect.right <= (window.innerWidth || html.clientWidth)
+      rect.left >= 0
     );
   }
 
