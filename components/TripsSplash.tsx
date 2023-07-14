4c08ba3ed3c506aea195401a7630f3ca412dbdae
Stephanie Chou <6627351+Stephanie-Chou@users.noreply.github.com>
2023-07-14 15:24:45 -0700

    trip splash use spash page component

--- components/TripsSplash.tsx
+++ components/TripsSplash.tsx
@@ -1,26 +1,12 @@
 import { ReactElement } from "react";
-import CanvasBackground from "../components/CanvasBackground";
-import styles from "../pages/about.module.css";
-import PageWrapper from "../components/PageWrapper";
+import SplashPage from "./SplashPage";
 
 export default function TripsSplash(): ReactElement {
   return (
-    <PageWrapper>
-      <div className={styles.about}>
-        <CanvasBackground>
-          <div className={styles.canvas_contents}>
-            <div className={styles.textLogo}><img src="/Trippintextlogo.svg" alt="Trippin Text Logo" /></div>
-            <div className={styles.aboutText}>
-              <h4>Trips</h4>
-              <p>Are you looking for the trip you shared? Check your emails for the link.</p>
-              <p>Want to create a new trip? Get inspired, <a href="/">Get Trippin</a>.</p>
-            </div>
-            <img className={styles.splashBanner} src="/Trippinsplashbanner.svg" alt="Trippin Splash Banner: Let's Get Trippin." />
-          </div>
-
-        </CanvasBackground>
-      </div>
-    </PageWrapper>
-
+    <SplashPage>
+      <h4>Trips</h4>
+      <p>Are you looking for the trip you shared? Check your emails for the link.</p>
+      <p>Want to create a new trip? Get inspired, <a href="/">Get Trippin</a>.</p>
+    </SplashPage>
   )
 }
